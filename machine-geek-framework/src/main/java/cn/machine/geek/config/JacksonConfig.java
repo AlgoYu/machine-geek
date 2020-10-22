479bf1077783d0c2ada41909b0a3d2a5e48b7391
MachineGeek <794763733@qq.com>
2020-10-22 20:49:59 +0800

    solve the time format problem.

--- machine-geek-framework/src/main/java/cn/machine/geek/config/JacksonConfig.java
+++ machine-geek-framework/src/main/java/cn/machine/geek/config/JacksonConfig.java
@@ -1,8 +1,22 @@
 package cn.machine.geek.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalTimeSerializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
+import static ch.qos.logback.core.rolling.helper.DateTokenConverter.DEFAULT_DATE_PATTERN;
 
 /**
  * @Author: MachineGeek
@@ -11,6 +25,9 @@ import org.springframework.context.annotation.Configuration;
  */
 @Configuration
 public class JacksonConfig {
+    private static final String DEFAULT_DATE_TIME_PATTERN = "yyyy-MM-dd HH:mm:ss";
+    private static final String DEFAULT_DATE_PATTERN = "yyyy-MM-dd";
+    private static final String DEFAULT_TIME_PATTERN = "HH:mm:ss";
     /** @Author: MachineGeek
      * @Description: 注册Jackson序列化类
      * @Date: 2020/10/6
@@ -19,6 +36,15 @@ public class JacksonConfig {
      */
     @Bean
     public ObjectMapper objectMapper(){
-        return new ObjectMapper();
+        ObjectMapper objectMapper = new ObjectMapper();
+        JavaTimeModule javaTimeModule = new JavaTimeModule();
+        javaTimeModule.addSerializer(LocalDateTime.class, new LocalDateTimeSerializer(DateTimeFormatter.ofPattern(DEFAULT_DATE_TIME_PATTERN)));
+        javaTimeModule.addSerializer(LocalDate.class, new LocalDateSerializer(DateTimeFormatter.ofPattern(DEFAULT_DATE_PATTERN)));
+        javaTimeModule.addSerializer(LocalTime.class, new LocalTimeSerializer(DateTimeFormatter.ofPattern(DEFAULT_TIME_PATTERN)));
+        javaTimeModule.addDeserializer(LocalDateTime.class, new LocalDateTimeDeserializer(DateTimeFormatter.ofPattern(DEFAULT_DATE_TIME_PATTERN)));
+        javaTimeModule.addDeserializer(LocalDate.class, new LocalDateDeserializer(DateTimeFormatter.ofPattern(DEFAULT_DATE_PATTERN)));
+        javaTimeModule.addDeserializer(LocalTime.class, new LocalTimeDeserializer(DateTimeFormatter.ofPattern(DEFAULT_TIME_PATTERN)));
+        objectMapper.registerModule(javaTimeModule);
+        return objectMapper;
     }
 }
--- machine-geek-system/src/main/java/cn/machine/geek/entity/Authority.java
+++ machine-geek-system/src/main/java/cn/machine/geek/entity/Authority.java
@@ -17,6 +17,10 @@ public class Authority implements GrantedAuthority {
         this.authority = authority;
     }
 
+    public void setAuthority(String authority) {
+        this.authority = authority;
+    }
+
     @Override
     public String getAuthority() {
         return this.authority;
