535206018c8a2d23ba26d3afe532c305d65808a8
MachineGeek <794763733@qq.com>
2020-11-11 12:14:13 +0800

    add delete api of system exception.

--- machine-geek-system/src/main/java/cn/machine/geek/controller/SystemExceptionController.java
+++ machine-geek-system/src/main/java/cn/machine/geek/controller/SystemExceptionController.java
@@ -3,14 +3,13 @@ package cn.machine.geek.controller;
 import cn.machine.geek.dto.PageRequest;
 import cn.machine.geek.dto.R;
 import cn.machine.geek.service.ISystemExceptionService;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * @Author: MachineGeek
@@ -29,5 +28,19 @@ public class SystemExceptionController {
     @PreAuthorize("hasAuthority('DEVELOP:SYSTEMEXCEPTION:GET')")
     public R paging(@Validated PageRequest pageRequest){
         return R.ok(systemExceptionService.paging(pageRequest.getPage(),pageRequest.getSize(),pageRequest.getKeyWord()));
+    }
+
+    @ApiOperation(value = "清空异常信息",notes = "清空异常信息")
+    @DeleteMapping(value = "/deleteById")
+    @PreAuthorize("hasAuthority('DEVELOP:SYSTEMEXCEPTION:DELETE')")
+    public R deleteById(@RequestParam(value = "id") Long id){
+        return R.ok(systemExceptionService.removeById(id));
+    }
+
+    @ApiOperation(value = "清空异常信息",notes = "清空异常信息")
+    @DeleteMapping(value = "/clear")
+    @PreAuthorize("hasAuthority('DEVELOP:SYSTEMEXCEPTION:CLEAR')")
+    public R clear(){
+        return R.ok(systemExceptionService.remove(new QueryWrapper<>()));
     }
 }
