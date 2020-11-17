166883a206afd7293d69d3a38db5ccc3bffeb274
MachineGeek <794763733@qq.com>
2020-11-17 12:30:12 +0800

    add system dictionary.

---
+++ machine-geek-system/src/main/java/cn/machine/geek/service/ISystemDictionaryService.java
@@ -0,0 +1,10 @@
+package cn.machine.geek.service;
+
+/**
+ * @Author: MachineGeek
+ * @Description: 系统字典服务
+ * @Email: 794763733@qq.com
+ * @Date: 2020/11/17
+ */
+public interface ISystemDictionaryService {
+}
---
+++ machine-geek-system/src/main/java/cn/machine/geek/service/impl/SystemDictionaryServiceImpl.java
@@ -0,0 +1,12 @@
+package cn.machine.geek.service.impl;
+
+import cn.machine.geek.service.ISystemDictionaryService;
+
+/**
+ * @Author: MachineGeek
+ * @Description:
+ * @Email: 794763733@qq.com
+ * @Date: 2020/11/17
+ */
+public class SystemDictionaryServiceImpl implements ISystemDictionaryService {
+}
